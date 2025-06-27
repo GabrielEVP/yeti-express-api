e9eb4fdc8f4f754a36bda135fd24ea41c83ff362
Gabriel Enrique Vargas Perez <98557334+GabrielEVP@users.noreply.github.com>
2025-06-28 01:35:54 +0200

    fix: Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://yeti-express-app-production.up.railway.app/', 'https://yeti-express-app.vercel.app'],
+    'allowed_origins' => ['https://yeti-express-app-production.up.railway.app', 'https://yeti-express-app.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
